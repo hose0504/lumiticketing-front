55806f760f75a73c7f65d0438f893bc593af5c1e
wish <hose0504@naver.com>
2025-04-02 16:59:42 +0900

    04023

--- src/main/java/com/care/boot/HomeController.java
+++ src/main/java/com/care/boot/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
             return "redirect:https://login.lumiticketing.click/boot/login";
         }
 
-        return "jsp/index";   // 너희 index.jsp 경로 확인 필요
+        return "index";   // 너희 index.jsp 경로 확인 필요
     }
 	
 	@RequestMapping("header")
