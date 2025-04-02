d17006f05c2aea653e77c9dab62df4ecce4ef715
wish <hose0504@naver.com>
2025-04-02 23:11:26 +0900

    04025

--- Scripts/deploy/deploy.sh
+++ Scripts/deploy/deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+# boot.war가 존재하는지 확인
+if [ -f /home/ubuntu/boot.war ]; then
+    sudo cp /home/ubuntu/boot.war /opt/tomcat/tomcat-10/webapps/
+else
+    echo "boot.war 파일이 존재하지 않습니다!"
+    exit 1
+fi
+
+# 톰캣 재시작
 sudo systemctl restart tomcat
+
+# 성공적으로 종료
+exit 0
